b05168b1d35eb767c56f35108b4ad3932bf6925f
Jan Urban <honza@urban-cz.eu>
2021-02-05 20:18:05 +0100

    fixed terraform scripts

--- tf-droplet/droplets.tf
+++ tf-droplet/droplets.tf
@@ -1,26 +1,21 @@
-provider "digitalocean" {}
-
-variable "vm_count" {
-  default = 2
-}
-
-data "digitalocean_image" "canarytrace" {
-  name = "canarytrace-virtual"
-}
-
-resource "digitalocean_droplet" "canarytrace" {
-  count = var.vm_count
-
-  image    = data.digitalocean_image.canarytrace.id
-  name     = "canarytrace${count.index}"
+resource "digitalocean_droplet" "docker-perf-server" {
+  image    = "docker-20-04"
+  name     = "docker-perf-server"
   region   = "fra1"
   size     = "s-4vcpu-8gb"
   monitoring = true
-
+  ssh_keys = [
+      data.digitalocean_ssh_key.terraform.id
+    ]
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.pvt_key)
+    timeout = "2m"
+  }
 }
 
 output "ip" {
-  value = [
-    for instance in digitalocean_droplet.canarytrace: instance.ipv4_address
-  ]
+  value = digitalocean_droplet.docker-perf-server.ipv4_address
 }
